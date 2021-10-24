8a80517be2703d159d3c5673db60d714949339ac
Alvin Wanjala <alvyynm@gmail.com>
2021-10-24 15:41:09 +0300

    Create userScore and computerScore variables

--- js/script.js
+++ js/script.js
@@ -24,6 +24,11 @@ function computerPlay() {
 
 let playerSelection;
 
+//Create and initialize a varible to store user score and computer score
+
+let userScore = 0;
+let computerScore = 0;
+
 //Create new function game that allows the player to play a 5 round game that keeps score and reports a winner or loser at the end. 
 
 function game () {
@@ -42,27 +47,54 @@ function game () {
 
         // Plays one round of the game and determines who won the game: player or computer
 
-        function playRound (playerSelection, computerSelecion) {
+    function playRound (playerSelection, computerSelecion) {
 
-                    if (playerSelection === computerSelecion) {
-                        return "It's a tie! Try again";
-                    } else if (computerSelecion === "ROCK" && playerSelection === "SCISSORS") {
-                        return "You Lose! Rock beats Scissors";
-                    } else if (computerSelecion === "ROCK" && playerSelection === "PAPER") {
-                        return "You Win! Paper beats Rock";
-                    } else if (computerSelecion === "SCISSORS" && playerSelection === "PAPER") {
-                        return "You Lose! Scissors beats Paper";
-                    } else if (computerSelecion === "SCISSORS" && playerSelection === "ROCK") {
-                        return "You Win! Rock beats Scissors";
-                    } else if (computerSelecion === "PAPER" && playerSelection === "ROCK") {
-                        return "You Lose! Paper beats ROCK";
-                    } else if (computerSelecion === "PAPER" && playerSelection === "SCISSORS") {
-                        return "You Win! Scissors beats Paper";
-                    }
+                if (playerSelection === computerSelecion) {
+                    userScore+=(1/2); 
+                    computerScore+=(1/2);   
+                    return "It's a tie! Try again";
+                        
+                 } else if (computerSelecion === "ROCK" && playerSelection === "SCISSORS") {
+                    computerScore++;    
+                    return "You Lose! Rock beats Scissors";
+                        
+                 } else if (computerSelecion === "ROCK" && playerSelection === "PAPER") {
+                    userScore++;    
+                    return "You Win! Paper beats Rock";
+                        
+                 } else if (computerSelecion === "SCISSORS" && playerSelection === "PAPER") {
+                    computerScore++;    
+                    return "You Lose! Scissors beats Paper";
+                        
+                 } else if (computerSelecion === "SCISSORS" && playerSelection === "ROCK") {
+                    userScore++;    
+                    return "You Win! Rock beats Scissors";
+                        
+                 } else if (computerSelecion === "PAPER" && playerSelection === "ROCK") {
+                    computerScore++;   
+                    return "You Lose! Paper beats ROCK";
+                        
+                 } else if (computerSelecion === "PAPER" && playerSelection === "SCISSORS") {
+                    userScore++; 
+                    return "You Win! Scissors beats Paper";
+                        
+                 } else {
+                     return "Please input 'Rock, Scissors, or 'Paper' without quotation marks";
+                 }
                 }
 
-                    console.log(playRound(playerSelection, computerSelection));
+                console.log(playRound(playerSelection, computerSelection));
             }
+        
+            if (userScore > computerScore) {
+                
+                alert("Congratulations!! You win the game");
+            } else if (userScore = computerScore) {
+                alert("Draw!! Play again.");
+            } else {
+                alert("You lose. Please try again.");
+            }
+        console.log(score);
         
     }
 
