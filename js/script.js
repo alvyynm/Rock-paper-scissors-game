6f85bd4934eb6766992c255fc08da52fccc43fa2
Alvin Wanjala <alvyynm@gmail.com>
2021-11-08 06:28:35 +0300

    Fix computer selection bug

--- js/script.js
+++ js/script.js
@@ -1,92 +1,79 @@
+// Variables
 
-function computerPlay() {
-    
-    // Create new variable and assign random number between 1 and 3 (incl.)
-
-    let computerPick;
-    computerPick = Math.floor(Math.random()*3) + 1;
-
-    // console.log(computerPick);
-
-    //Assign Rock, Paper, Scissors to number 1, 2 and 3, respectively
-
-    if (computerPick === 1) {
-        return "ROCK";
-    } else if (computerPick === 2){
-        return "PAPER";
-    } else {
-        return "SCISSORS";
-    }
-}
-
-
-//Create new variable to store player selection
-
+let computerPick;
 let playerSelection;
-
-//Create and initialize a varible to store user score and computer score
-
+let computerSelection;
 let userScore = 0;
 let computerScore = 0;
 
-//Creates new variable to store computer selection
+//Create and initialize a varible to store user score and computer score
 
-let computerSelection = computerPlay();
-
-//Create new function game that allows the player to play a 5 round game that keeps score and reports a winner or loser at the end. 
+//Creates new function that allows the player to play a 5 round game that keeps score and reports a winner or loser at the end. 
 
 function game () {
 
-    // playerSelection = prompt("Enter your selection: Rock, Scissors, or Paper:").toUpperCase();
-
-    for (let i = 1; i <= 5; i++) {
         //Creates new constant and adds an event listener
 
         const buttons = document.querySelectorAll('button');
 
         buttons.forEach((button) => {
-
-            // and for each one we add a 'click' listener
             button.addEventListener('click', () => {
                 playerSelection = (button.id).toUpperCase();
-                // console.log(playerSelection);
+                console.log(computerSelection);
+
+
+                function computerPlay() {
+    
+                    // Create new variable and assign random number between 1 and 3 (incl.)
+                    computerPick = Math.floor(Math.random() * 3);
+                    console.log(computerPick);
+                    //Assign Rock, Paper, Scissors to number 1, 2 and 3, respectively
                 
-                // Plays one round of the game and determines who won the game: player or computer
+                    if (computerPick === 0) {
+                        return "ROCK";
+                    } else if (computerPick === 1) {
+                        return "PAPER";
+                    } else if (computerPick === 2) {
+                        return "SCISSORS";
+                    }
+                };
+                
+                //Create new variable to store player selection and computer selection
+                
+                computerSelection = computerPlay();
+                
+ // Plays one round of the game and determines who won the game: player or computer
 
-        function playRound (playerSelection, computerSelecion) {
+        function playRound (playerSelection, computerSelection) {
 
-            if (playerSelection === computerSelecion) {
-                userScore+=(1/2); 
-                computerScore+=(1/2);   
+
+            if (playerSelection === computerSelection) {  
                 return "It's a tie! Try again";
                     
-            } else if (computerSelecion === "ROCK" && playerSelection === "SCISSORS") {
+            } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
                 computerScore++;    
                 return "You Lose! Rock beats Scissors";
                     
-            } else if (computerSelecion === "ROCK" && playerSelection === "PAPER") {
+            } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
                 userScore++;    
                 return "You Win! Paper beats Rock";
                     
-            } else if (computerSelecion === "SCISSORS" && playerSelection === "PAPER") {
+            } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
                 computerScore++;    
                 return "You Lose! Scissors beats Paper";
                     
-            } else if (computerSelecion === "SCISSORS" && playerSelection === "ROCK") {
+            } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
                 userScore++;    
                 return "You Win! Rock beats Scissors";
                     
-            } else if (computerSelecion === "PAPER" && playerSelection === "ROCK") {
+            } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
                 computerScore++;   
                 return "You Lose! Paper beats Rock";
                     
-            } else if (computerSelecion === "PAPER" && playerSelection === "SCISSORS") {
+            } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
                 userScore++; 
                 return "You Win! Scissors beats Paper";
-                    
-            } else {
-                return "Please input 'Rock, Scissors, or 'Paper' without quotation marks";
-            }
+                };
             };
 
             const div = document.querySelector('div');
@@ -95,29 +82,33 @@ function game () {
             console.log(userScore);
             console.log(computerScore);
 
+            function updateScore() {
+                document.getElementById("pScore").textContent = computerScore;
+                document.getElementById("cScore").textContent = userScore;
+              }
+              
+              updateScore();
             // console.log(playRound(playerSelection, computerSelection));
             });
+ // Determine who wins the game
+     
+    //   const finalScore = document.querySelector('.finalresults');
+ 
+        // if (userScore > computerScore) {
+
+        //     // finalScore.textContent = "Congratulations!! You win the game";
+                
+        //     alert("Congratulations!! You win the game");
+        //     } else if (userScore = computerScore) {
+        //         // finalScore.textContent = "Draw!! Play again.";
+        //         alert("Draw!! Play again.");
+        //     } else if (userScore < computerScore) {
+        //         // finalScore.textContent = "You lose. Please try again.";
+        //         alert("You lose. Please try again.");
+        //     };
+
           });
 
-        };
-        
-        // Determine who wins the game
-        const finalScore = document.querySelector('.finalresults');
+    };    
 
-        
-        if (userScore > computerScore) {
-
-            finalScore.textContent = "Congratulations!! You win the game";
-                
-            // alert("Congratulations!! You win the game");
-            } else if (userScore = computerScore) {
-                finalScore.textContent = "Draw!! Play again.";
-                // alert("Draw!! Play again.");
-            } else {
-                finalScore.textContent = "You lose. Please try again.";
-                // alert("You lose. Please try again.");
-            };
-        
-    };
-
-    console.log(game());
+game();
